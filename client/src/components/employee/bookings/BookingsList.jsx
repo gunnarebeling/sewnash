bceaf451c719efb9eb3d0a807449787a79197d41
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-12-30 11:56:15 -0600

    made a session details component

--- client/src/components/employee/bookings/BookingsList.jsx
+++ client/src/components/employee/bookings/BookingsList.jsx
@@ -5,10 +5,12 @@ import { enUS } from 'date-fns/locale';
 import { useEffect, useState } from 'react';
 import { getAllSessions } from '../../../managers/sessionManager';
 import { setTimeFromString } from '../../../managers/FormatFunctions';
+import { useNavigate } from 'react-router-dom';
 
 export const BookingsList = () => {
     const [sessions, setSessions] = useState([])
     const [events, setEvents] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         getAllSessions().then(setSessions)
@@ -16,18 +18,22 @@ export const BookingsList = () => {
 
     useEffect(() => {
        const events = sessions.map(s => {
-        let newDate = setTimeFromString(new Date(s.dateTime), s.time?.startTime); 
-        const endDate = new Date(newDate); // Clone startDate
-        endDate.setHours(endDate.getHours() + s.sewClass.duration);
-
-        const event = {
+        if (s.bookings.length > 0) {
+          
+          let newDate = setTimeFromString(new Date(s.dateTime), s.time?.startTime); 
+          const endDate = new Date(newDate); // Clone startDate
+          endDate.setHours(endDate.getHours() + s.sewClass.duration);
+  
+          const event = {
+            id: s.id,
             title: `${!s.open ? `🔒` : "" } ${s.sewClass?.name}`,
             start: newDate,
             end: endDate,
-            
-
+              
+  
+          }
+          return event
         }
-        return event
        }) 
        setEvents(events)
     }, [sessions])
@@ -44,19 +50,20 @@ export const BookingsList = () => {
       locales,
     });
     
-    
+    const handleEventClick = (e) => {
+      navigate(`session/${e.id}`)
+
+
+    }
 
     return (
-        
-
-
-  
     <Calendar
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 1000 }}
+      onSelectEvent={handleEventClick}
       
     />
   );
