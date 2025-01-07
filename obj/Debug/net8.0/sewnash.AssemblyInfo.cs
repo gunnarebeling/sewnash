1eb7740387c9749182791d9f873caf04e1c6a9b8
gunnarebeling <gunnarebeling@icloud.com>
2025-01-06 19:03:53 -0600

    need to update

--- obj/Debug/net8.0/sewnash.AssemblyInfo.cs
+++ obj/Debug/net8.0/sewnash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sewnash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b82daa2f25f38358986a7ec9b55f98f04df8dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09359f58e715944162ac324327df07515455982a")]
 [assembly: System.Reflection.AssemblyProductAttribute("sewnash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sewnash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
